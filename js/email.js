5806bf77d09fc63e7e80a77721795e9d4e638eb4
Danro19 <danrodri1911@gmail.com>
2025-04-28 15:07:17 -0500

    Terminando Formulario y funcionalidad

--- js/email.js
+++ js/email.js
@@ -1,12 +1,16 @@
-document.getElementById("contact-form").addEventListener("submit", function(e) {
+window.addEventListener('load', function() {
+  emailjs.init('kBZ8EPMogUnhwf9rj'); // Inicializar después de que todo cargue
+
+  document.getElementById("contact-form").addEventListener("submit", function(e) {
     e.preventDefault();
-  
-    emailjs.sendForm('service_oe2f0fr', 'template_7xux6xh', this)
-      .then(() => {
+    
+    emailjs.sendForm('service_oe2f0fr', 'template_48mj33e', this)
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
         alert("Mensaje enviado exitosamente 💌");
-      }, (error) => {
-        alert("Error al enviar el mensaje 😥");
-        console.log(error);
+      }, function(error) {
+        console.error('FAILED...', error);
+        alert("Error al enviar el mensaje 😥 Revisa la consola para más detalles.");
       });
   });
-  
+});
